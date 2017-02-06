7efd767c6f3c30e76222863c95cbc5181f23780d
AndroidStorm <williamg.dev@gmail.com>
2017-02-06 17:52:42 -0600

    Improvement Player Activity

--- app/src/main/java/com/github/williamgdev/examples/simplewowza/MainActivity.java
+++ app/src/main/java/com/github/williamgdev/examples/simplewowza/MainActivity.java
@@ -1,5 +1,6 @@
 package com.github.williamgdev.examples.simplewowza;
 
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -70,11 +71,6 @@ public class MainActivity extends AppCompatActivity implements WZStatusCallback 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // SetUp Vitamio Library
-        if(!LibsChecker.checkVitamioLibs(this)) {
-            Log.d(TAG, "onCreate: Error Initializing Vitamio...");
-            return;
-        }
 
         setContentView(R.layout.activity_main);
 
@@ -121,9 +117,6 @@ public class MainActivity extends AppCompatActivity implements WZStatusCallback 
         broadcastButton = (Button) findViewById(R.id.broadcast_button);
         playButton = (Button) findViewById(R.id.play_button);
         stopButton = (Button) findViewById(R.id.stop_button);
-
-        videoView = (VideoView) findViewById(R.id.buffer);
-        centerLayout = (io.vov.vitamio.widget.CenterLayout) findViewById(R.id.video_layer);
     }
 
     private void toggleBroadcast() {
@@ -252,159 +245,10 @@ public class MainActivity extends AppCompatActivity implements WZStatusCallback 
         toggleBroadcast();
     }
 
-    public void playFromPath(View view){
-        path = "https://c2a38e.entrypoint.cloud.wowza.com/app-2343/ngrp:13e3b911_all/playlist.m3u8";
-        centerLayout.setVisibility(View.VISIBLE);
 
-        videoView.setVideoURI(Uri.parse(path));
-        videoView.setMediaController(new MediaController(this));
-        videoView.requestFocus();
-        videoView.setOnInfoListener(new io.vov.vitamio.MediaPlayer.OnInfoListener() {
-            @Override
-            public boolean onInfo(io.vov.vitamio.MediaPlayer mp, int what, int extra) {
-                switch (what) {
-                    case MediaPlayer.MEDIA_INFO_BUFFERING_START:
-                        if (videoView.isPlaying()) {
-                            videoView.pause();
-
-                        }
-                        break;
-                    case MediaPlayer.MEDIA_INFO_BUFFERING_END:
-                        videoView.start();
-                        break;
-                }
-                return true;
-            }
-        });
-        videoView.setOnBufferingUpdateListener(new io.vov.vitamio.MediaPlayer.OnBufferingUpdateListener() {
-            @Override
-            public void onBufferingUpdate(io.vov.vitamio.MediaPlayer mp, int percent) {
-                Log.d(TAG, "onBufferingUpdate: Percent: " + percent + "%");
-            }
-        });
-        videoView.setOnPreparedListener(new io.vov.vitamio.MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(io.vov.vitamio.MediaPlayer mediaPlayer) {
-                // optional need Vitamio 4.0
-                mediaPlayer.setPlaybackSpeed(1.0f);
-            }
-        });
-        videoView.setOnErrorListener(new io.vov.vitamio.MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(io.vov.vitamio.MediaPlayer mp, final int what, int extra) {
-                new Handler(Looper.getMainLooper()).post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, "Error: " + what, Toast.LENGTH_SHORT).show();
-                    }
-                });
-                return true;
-            }
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        videoView.setMediaController(new MediaController(this));
-//        videoView.setVideoURI(Uri.parse(path));
-//        videoView.requestFocus();
-//        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-//            public void onPrepared(MediaPlayer mp) {
-//                mp.start();
-//                new Handler(Looper.getMainLooper()).post(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Toast.makeText(MainActivity.this, "Play Music", Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//            }
-//        });
-
-//        mediaPlayer = new MediaPlayer();
-//        try {
-//
-//            mediaPlayer.setDataSource(path);
-//            mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
-//
-//                @Override
-//                public void onBufferingUpdate(MediaPlayer mp, int percent) {
-//
-//                    // TODO Auto-generated method stub
-//                    if(mediaPlayer.isPlaying()){
-//                        is_playing_firstTime = true;
-//                    }else{
-//                        System.out.println("MP stopped");
-//                    }
-//                    System.out.println(percent + "% 0");
-//                    if(isFirst_Loading_Complete) {
-//                        System.out.println(percent + "% 1");
-//                    }
-//                    if (percent < 5) {
-//                        mediaPlayer.pause();
-//                        isStart = true;
-//                        if(isFirst_Loading_Complete) {
-//                            System.out.println(percent + "% 2");
-//                        }
-//                    }
-//                    else if (percent < 20) {
-//                        // loadRateView.setText(percent + "%");
-//                        // allows the user to actively aware Play
-//                    } else { // > 20%
-//                        mediaPlayer.start();
-//                        System.out.println(percent + "% 3");
-//                        isFirst_Loading_Complete = true;// true because first loading is displayed
-////                    mediaPlayer.setBufferSize(5000);
-//                        System.out.println(percent + "% 4");
-//                    }
-//                }
-//            });
-//            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-//                @Override
-//                public void onPrepared(MediaPlayer mp) {
-//                    mediaPlayer.start();
-//                    new Handler(Looper.getMainLooper()).post(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            Toast.makeText(MainActivity.this, "Music Playing now", Toast.LENGTH_SHORT).show();
-//                        }
-//                    });
-//                }
-//            });
-//            mediaPlayer.prepare();
-//            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-//                @Override
-//                public void onCompletion(MediaPlayer mp) {
-//                    mediaPlayer.release();
-//
-//                    new Handler(Looper.getMainLooper()).post(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            Toast.makeText(MainActivity.this, "Music Stop", Toast.LENGTH_SHORT).show();
-//                        }
-//                    });
-//                }
-//            });
-//            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-//        }
-////        mediaPlayer.setBufferSize(5000); //set buffer size
-//        isFirst_Loading_Complete = false;
-
-    }
-
-    @Override
-    protected void onPause() {
+    public void openPlayer(View view) {
+        Intent intent = new Intent(this, PlayerActivity.class);
+        startActivity(intent);
 
     }
 }
